313a9a7beb8b56af8cbafd8bfd343cc754137cde
Praniuk <46418147+Praniuk@users.noreply.github.com>
2024-11-21 17:55:26 +0100

    Update 1_Scraper_pracujpl.py

--- 1_Scraper_pracujpl.py
+++ 1_Scraper_pracujpl.py
@@ -2,51 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# URL of the page to scrape
+
 base_url = 'https://it.pracuj.pl/praca'
-
-# Send a GET request to the base URL
 response = requests.get(base_url)
-
-# Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.content, 'html.parser')
-
-# Find the maximum page number
 max_page_number = int(soup.find('span', attrs={'data-test': 'top-pagination-max-page-number'}).text)
-
-
 
 with open('counts.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=';')
     writer.writerow(['Location', 'Specializaion', 'Position_level', 'Contract_type', 'Work_schedule', 'Work_modes'])
 
-# Iterate over each page
 for page_number in range(1, max_page_number + 1):
-    # Construct the URL for the current page
+   
     url = f'{base_url}?pn={page_number}'
-
     print(f"Scraping page {page_number}...")
-
-    # Send a GET request to the URL
     response = requests.get(url)
-
-    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Find all <a> tags with 'data-test' attribute equal to 'link-offer'
     offer_links = soup.find_all('a', attrs={'data-test': 'link-offer'})
 
-    # Iterate over each offer link
     for link in offer_links:
         offer_url = link['href']
-
-        # Send a GET request to the offer URL
         offer_response = requests.get(offer_url)
-
-        # Parse the HTML content of the offer page
         offer_soup = BeautifulSoup(offer_response.content, 'html.parser')
 
-        # Find the element with 'data-test' attribute equal to 'sections-benefit-contracts-text'
         if offer_soup.find('div', attrs={'data-test': 'text-benefit'}) is None:
             benefit_location_text = ''
         else:
